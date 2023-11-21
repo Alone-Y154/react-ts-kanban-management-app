6dd504e7effc04aa18505a8b00920e599a6c66f1
Alone-Y154 <yashwanthkris154@gmail.com>
2023-11-22 01:29:17 +0530

    Edit and Delete UI Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import Column from "./Components/Column";
+import EditandDelete from "./Components/EditandDeleteCard";
 import Navbar from "./Components/Navbar";
 
 function App() {
-  return( 
-  <div className="flex flex-col h-screen">
-    <Navbar />
-    <div className="bg-grey-500 h-screen w-full p-6 overflow-x-scroll no-scrollbar scroll-smooth">
-    <Column />
+  return (
+    <div className="flex flex-col h-screen bg-grey-500">
+      <Navbar />
+      <EditandDelete />
+      <div className="bg-grey-500 h-screen w-full p-6 overflow-x-scroll no-scrollbar scroll-smooth">
+        <Column />
+      </div>
     </div>
-    </div>
-    );
+  );
 }
 
 export default App;
---
+++ src/Components/EditandDeleteCard.tsx
@@ -0,0 +1,12 @@
+
+
+const EditandDelete = () => {
+  return (
+    <div className="w-[192px] p-4 gap-4 h-[92px] rounded-lg bg-grey-400 shadow-[0px_10px_20px_0px_rgba(54,_78,_126,_0.25)]">
+        <p className="text-grey-700 text-[13px] font-medium leading-[23px]">Edit Board</p>
+        <p className="text-danger-700 text-[13px] font-medium leading-[23px]">Delete Board</p>
+    </div>
+  )
+}
+
+export default EditandDelete
